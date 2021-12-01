483608f739b885486a1cc8293a8057c72490f557
Dickson Yu <dickson.yu@email.com>
2021-12-01 14:07:39 -0500

    added exit function

--- play.js
+++ play.js
@@ -1,1 +1,17 @@
 const { connect } = require("./client");
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+  
+  const handleUserInput = (data, () => {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  });
+  
+  return stdin;
+};
